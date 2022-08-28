1a7cffb7624fcd17c07b79e7b9925b3ed4dca514
jeferson-rpo <98125058+jeferson-rpo@users.noreply.github.com>
2022-08-28 00:28:24 -0500

    en el lugar indicado

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,23 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	
+	
+	
+	public void main(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getnombre () {
+		return nombre;
+	}
+	
+	public void setnombre(String nombre ) {
+		this.nombre = nombre;
+	}
+
+
+	
+
+}
