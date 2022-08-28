7b38bb91510054c503d4605558737bdbf7a4b679
jeferson-rpo <98125058+jeferson-rpo@users.noreply.github.com>
2022-08-28 00:25:41 -0500

    programa primera entrega
    1ra entrega

---
+++ src/taller3/televisores/Control.java
@@ -0,0 +1,45 @@
+package taller3.televisores;
+
+public class Control {
+	TV tv;
+	
+	
+	public void turnOn() {
+		tv.turnOn();		
+	}
+	public void turnOff() {
+
+		tv.turnOff();
+	}
+	public void canalUp() {
+
+		tv.canalUp();
+	}
+	public void canalDown() {
+
+		tv.canalDown();
+	}
+	public void volumenUp() {
+
+		tv.volumenUp();
+	}
+	public void volumenDown() {
+		
+		tv.volumenDown();
+	}
+	public TV setTv() {
+		return tv;
+
+	}
+	public void getTv(TV tv) {
+		this.tv = tv;
+
+	}
+	
+	void enlazar(TV tv) {
+		this.tv = tv;
+		tv.setControl(this);
+	}
+	
+	
+}
